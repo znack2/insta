4162f34f4021cef30bc280ca7732d07a2460c1dd
andreysukhov <andrey@kr.digital>
2020-02-09 01:11:26 +0300

    loader

---
+++ src/components/Preloader/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import style from './preloader.module.css'
+
+const Preloader = () => {
+    return <div className={style['lds-dual-ring']} />
+}
+
+export default Preloader
--- src/pages/v1/index.js
+++ src/pages/v1/index.js
@@ -2,10 +2,26 @@ import React, { useState } from 'react';
 
 import Btn from '../../components/Btn'
 
+import Preloader from '../../components/Preloader'
+
 const FirstProto = () => {
 
 
     const [accepted, setAccept] = useState(false)
+    const [loading, setLoading] = useState(false)
+
+
+    if (loading) {
+        return (
+            <div className={'tc'} style={{'maxWidth': '400px', 'margin': '20px auto'}}>
+                <Preloader />
+                <br/>
+                <p>
+                    Собираю данные
+                </p>
+            </div>
+        )
+    }
 
     if (accepted) {
         return (
@@ -20,12 +36,36 @@ const FirstProto = () => {
         <div className="tc">
             <Btn onClick={() => {
 
-                const newWin = window.open("about:blank", "auth", "width=500,height=500");
+                const w = 500
+                const h = 500
+
+                const left = (window.width/2)-(w/2);
+                const top = (window.height/2)-(h/2);
+
+                const newWin = window.open('about:blank', "auth", `toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=no, copyhistory=no, width=${w}, height=${h}, top=${top}, left=${left}`);
 
                 newWin.document.write(
-                    `<div style="padding: 20px;">
+                    `<div style="padding: 20px;font-family: 'Open Sans', sans-serif">
+                        <div>
+                        <img alt="Instagram" style="max-width: 300px;"
+                            src="https://www.instagram.com/static/images/web/mobile_nav_type_logo-2x.png/1b47f9d0e595.png">
+                        </div>    
                         <p>
-                            Приложение запрашивает доступ к Вашему профилю в instagram
+                            Приложение запрашивает следующие разрешения:
+                        </p>
+                        
+                        <p>
+                        <strong>
+                            Доступ к информации в вашем профиле
+                        </strong>
+                       
+                        имя пользователя, тип аккаунта
+                        </p>
+                        <p>
+                        <strong>
+                            Доступ к вашим медиафайлам
+                        </strong>
+                            подписи, количество и тип медиафайлов, URL для доступа, постоянная ссылка, метка времени и миниатюра
                         </p>
                         <div style="display: flex;">
                             <button style="border: none;
@@ -51,16 +91,21 @@ const FirstProto = () => {
                     </div>`
                 );
 
-                newWin.document.getElementById("accept").addEventListener("click",(e) => {
-                    setAccept(true)
-                    newWin.close()
+                newWin.document.getElementById("accept").addEventListener("click",() => {
+                        setLoading(true)
+
+                        setTimeout(() => {
+                            setAccept(true)
+                            setLoading(false)
+                        }, 2000)
+
+                        newWin.close()
                 },false);
 
             }}>
                 Авторизация через инстаграм
             </Btn>
         </div>
-
     )
 }
 
