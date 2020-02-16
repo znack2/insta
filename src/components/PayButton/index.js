47d7b9a43be6770874a0c3a40df055eb081914f5
andreysukhov <andrey@kr.digital>
2020-02-16 15:12:08 +0300

    INSTA-4

--- src/components/PayButton/index.js
+++ src/components/PayButton/index.js
@@ -9,11 +9,11 @@ const PayButton = (props) => {
     const location = new URL(window.location)
     const data = PAYMENT_BUTTONS[location.searchParams.get('paymentButton')]
 
-    if (data.visibility === 'always' || (
+    if (data && (data.visibility === 'always' || (
         data.visibility === 'formLoaded' && props.formLoaded
     )|| (
         data.visibility === 'accepted' && props.accepted
-    )) {
+    ))) {
         return(
             <div className="tc" style={{padding: '20px'}}>
                 {data.text &&  <p>
